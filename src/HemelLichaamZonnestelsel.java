52ce89ded41768d7820c571c272d870dd409d472
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-21 07:05:33 +0200

    Planeten en/of hemellichamen raden in een lijst. ArrayList > Array!

---
+++ src/HemelLichaamZonnestelsel.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HemelLichaamZonnestelsel {
+    public static Scanner invoer = new Scanner(System.in);
+
+    public static String[] hemelLichamen = {"Zon", "Mercurius", "Venus", "Aarde", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus", "Pluto"};
+
+    public static String invoerNaam;
+
+    public static boolean alKlaar = false;
+
+}
--- src/NamenRaden.java
+++ src/NamenRaden.java
@@ -1,2 +1,28 @@
-public class NamenRaden {
+import java.util.Arrays;
+import java.util.List;
+
+public class NamenRaden extends HemelLichaamZonnestelsel {
+    public static void main(String[] args) {
+        while (!alKlaar) {
+            HemelLichaamZonnestelsel hemelLichaamZonnestelsel = new HemelLichaamZonnestelsel();
+
+            System.out.printf("%nNoem een planeet en/of hemellichaam in onze zonnestelsel:%n");
+            invoerNaam = invoer.nextLine();
+
+            List hemelLichamenList = Arrays.asList(hemelLichamen);
+
+
+            if (hemelLichamenList.contains(invoerNaam)) {
+                System.out.printf(invoerNaam + " staat in het lijstje op indexnummer: " + hemelLichamenList.indexOf(invoerNaam) + "%n");
+                alKlaar = false;
+            } else {
+                System.out.printf(invoerNaam + " was niet gevonden in lijstje!%n");
+                alKlaar = false;
+            }
+        }
+
+
+
+    }
+
 }
